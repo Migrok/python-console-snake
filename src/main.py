12bf796e6e02e2c8e334b3f49c4f9e1fd4de9366
Migrok <90355004+Migrok@users.noreply.github.com>
2024-05-16 01:11:52 +0300

    add move

--- src/main.py
+++ src/main.py
@@ -1,16 +1,40 @@
 import curses
 
 class Snake:
-    def __init__(self, length, speed, direction, current_coord):
+    def __init__(self, length, speed, direction, current_coords):
         self.length = length
         self.speed = speed
         self.direction = direction
-        self.current_coord = current_coord
+        self.current_coords = current_coords
 
     def draw(self, stdscr):
-        for i in self.current_coord:
-            if i == 0:
-                stdscr.addch(i[0], i[1], '+')
+        head = 0
+        for coord in self.current_coords:
+            if head == 0:
+                stdscr.addch(coord[0], coord[1], '@')
+                head = 1
+            else:
+                stdscr.addch(coord[0], coord[1], '#')
+
+    def move(self):
+        direct_1 = self.direction
+        for coord in self.current_coords:
+            direct_2 = coord[2]
+            coord[2] = direct_1
+            direct_1 = direct_2
+
+        for coord in self.current_coords:
+            if coord[2] == curses.KEY_RIGHT:
+                coord[1] += 1
+            elif coord[2] == curses.KEY_LEFT:
+                coord[1] -= 1
+            elif coord[2] == curses.KEY_UP:
+                coord[0] -= 1
+            elif coord[2] == curses.KEY_DOWN:
+                coord[0] += 1
+
+
+
 
 def borders(stdscr, height, width):
     for y in range(0, height):
@@ -22,6 +46,15 @@ def borders(stdscr, height, width):
             elif (y, x) in [(0, 0), (0, width - 1), (height - 1, 0), (height - 1, width - 1)]:
                 stdscr.addch(y, x, '+')
 
+def snake_init(stdscr, height, width):
+    snake = Snake(length=4, speed=1, direction=curses.KEY_LEFT, current_coords=[])
+    for i in range(snake.length):
+        x = width // 2
+        y = height // 2
+        snake.current_coords.append([y, x+i, curses.KEY_LEFT])
+    snake.draw(stdscr)
+    return snake
+
 def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(1)
@@ -29,17 +62,19 @@ def main(stdscr):
     height, width = 20, 40
     borders(stdscr, height, width)
 
+    snake = snake_init(stdscr, height, width)
 
-    snake = Snake(length=4, speed=1, direction='west', current_coord=[])
-    for i in range(snake.length):
-        x = width // 2
-        y = height // 2
-        snake.current_coord.append([y+i, x])
 
     while True:
         key = stdscr.getch()
         if key == ord('q'):
             break
+        elif key in [curses.KEY_RIGHT, curses.KEY_LEFT, curses.KEY_UP, curses.KEY_DOWN]:
+            snake.direction = key
+            snake.move()
+            stdscr.clear()
+            borders(stdscr, height, width)
+            snake.draw(stdscr)
         stdscr.refresh()
 
 curses.wrapper(main)
