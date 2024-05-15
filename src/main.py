9e9e6d8e544ee9c94690bc31368900751e7eeeb2
Migrok <90355004+Migrok@users.noreply.github.com>
2024-05-16 01:23:42 +0300

    add update_direction

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import curses
+import time
 
 class Snake:
     def __init__(self, length, speed, direction, current_coords):
@@ -33,8 +34,15 @@ class Snake:
             elif coord[2] == curses.KEY_DOWN:
                 coord[0] += 1
 
-
-
+    def update_direction(self, new_direction):
+        opposite_directions = {
+            curses.KEY_UP: curses.KEY_DOWN,
+            curses.KEY_DOWN: curses.KEY_UP,
+            curses.KEY_LEFT: curses.KEY_RIGHT,
+            curses.KEY_RIGHT: curses.KEY_LEFT
+        }
+        if new_direction != opposite_directions.get(self.direction):
+            self.direction = new_direction
 
 def borders(stdscr, height, width):
     for y in range(0, height):
@@ -43,38 +51,38 @@ def borders(stdscr, height, width):
                 stdscr.addch(y, x, '-')
             elif x == 0 or x == width - 1:
                 stdscr.addch(y, x, '|')
-            elif (y, x) in [(0, 0), (0, width - 1), (height - 1, 0), (height - 1, width - 1)]:
-                stdscr.addch(y, x, '+')
 
-def snake_init(stdscr, height, width):
+def snake_init(height, width):
     snake = Snake(length=4, speed=1, direction=curses.KEY_LEFT, current_coords=[])
     for i in range(snake.length):
         x = width // 2
         y = height // 2
-        snake.current_coords.append([y, x+i, curses.KEY_LEFT])
-    snake.draw(stdscr)
+        snake.current_coords.append([y, x + i, curses.KEY_LEFT])
     return snake
 
 def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(1)
+    stdscr.timeout(100)
 
     height, width = 20, 40
     borders(stdscr, height, width)
 
-    snake = snake_init(stdscr, height, width)
-
+    snake = snake_init(height, width)
 
     while True:
         key = stdscr.getch()
         if key == ord('q'):
             break
         elif key in [curses.KEY_RIGHT, curses.KEY_LEFT, curses.KEY_UP, curses.KEY_DOWN]:
-            snake.direction = key
-            snake.move()
-            stdscr.clear()
-            borders(stdscr, height, width)
-            snake.draw(stdscr)
+            snake.update_direction(key)
+
+        snake.move()
+        stdscr.clear()
+        borders(stdscr, height, width)
+        snake.draw(stdscr)
         stdscr.refresh()
+
+        time.sleep(0.1)
 
 curses.wrapper(main)
